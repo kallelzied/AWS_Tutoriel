85defc7dffda7cbe07ef7fdda4126f32b4261928
kallelzied <kallelzied@live.com>
2018-08-11 18:22:04 +0200

    read event from sns

--- serverless-sns/read_from_sns/read_from_sns.py
+++ serverless-sns/read_from_sns/read_from_sns.py
@@ -2,32 +2,19 @@ import json
 import logging
 import os
 import time
-import boto3
-
-sns = boto3.client('sns')
-USER_LAST_LOGIN_SNS_TOPIC_ARN = os.getenv('USER_LAST_LOGIN_SNS_TOPIC_ARN')
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
 def lambda_handler(event, context):
-    logger.info("Recording last login date")
-    user_id = event['user_id']
-    record_last_login_date(user_id)
+    logger.info("Read user_id and date from sns")
+    message = _parse_event(event)
+    logger.info(message)
 
 
-def record_last_login_date(user_id):
-    sns_message = {
-        'userId': int(user_id),
-        'date': int(time.time())
-    }
-    publish_to_sns(USER_LAST_LOGIN_SNS_TOPIC_ARN, sns_message)
-
-
-def publish_to_sns(topic_arn, message):
-    message= json.dumps(message)
-    logger.info("Message: {}".format(message))
-    response = sns.publish(TopicArn=topic_arn, Message=message)
-    log_msg = "Message published to SNS:\nMessage: {}\nResponse:\n{}".format(message, str(response))
-    logger.info(log_msg)
+def _parse_event(event):
+    message = event['Records'][0]['Sns']['Message']
+    message = json.loads(message)
+    return {'user_id': int(message['user_id']),
+            'date': int(message['date'])}
