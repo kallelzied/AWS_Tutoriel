e865fe3bf331c98e9b593a18a0e94451535a4888
kallelzied <kallelzied@live.com>
2018-08-12 19:17:36 +0200

    correction

--- serverless-s3/s3-get-object-python/s3-get-object-python.py
+++ serverless-s3/s3-get-object-python/s3-get-object-python.py
@@ -7,7 +7,7 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
 
     # Get the object from the event and show its content type
-    bucket = ['bucket']
+    bucket = event['bucket']
     key = urllib.unquote_plus(event['key'].encode('utf8'))
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
